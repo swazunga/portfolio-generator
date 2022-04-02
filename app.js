5ded92ebdcd8f076c2efc954e5687138af681608
Michael Kendrick <michael@locumsmart.net>
2022-04-02 16:41:29 -0500

    help

--- app.js
+++ app.js
@@ -1,14 +1,6 @@
 const inquirer = require("inquirer");
-// const fs = require("fs");
-// const generatePage = require("./src/page-template.js");
-
-// const pageHTML = generatePage(name, github);
-
-// fs.writeFile("./index.html", pageHTML, (err) => {
-//   if (err) throw err;
-
-//   console.log("Portfolio complete! Check out index.html to see the output!");
-// });
+const fs = require("fs");
+const generatePage = require("./src/page-template.js");
 
 const promptUser = () => {
   return inquirer.prompt([
@@ -137,6 +129,14 @@ const promptProject = (portfolioData) => {
 };
 promptUser()
   .then(promptProject)
-  .then((portfolioData) => {
-    console.log(portfolioData);
+  .then((portfoliotData) => {
+    const pageHTML = generatePage(portfolioData);
+
+    fs.writeFile("./index.html", pageHTML, (err) => {
+      if (err) throw new Error(err);
+
+      console.log(
+        "Page complete! Check out index.html in this directory to see it!"
+      );
+    });
   });
