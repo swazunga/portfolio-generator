ae7a555f29867e2f18be5e0ae6ef6436ba1b375f
Michael Kendrick <michael@locumsmart.net>
2022-04-03 14:23:10 -0500

    help

--- app.js
+++ app.js
@@ -130,13 +130,24 @@ const promptProject = (portfolioData) => {
 promptUser()
   .then(promptProject)
   .then((portfoliotData) => {
-    const pageHTML = generatePage(portfolioData);
-
-    fs.writeFile("./index.html", pageHTML, (err) => {
-      if (err) throw new Error(err);
-
-      console.log(
-        "Page complete! Check out index.html in this directory to see it!"
-      );
-    });
+    return generatePage(portfolioData);
+  })
+  .then((pageHTML) => {
+    return writeFile(pageHTML);
+  })
+  .then((writeFileResponse) => {
+    console.log(writeFileResponse);
+    return fs.copyFile();
+  })
+  .then((copyFileResponse) => {
+    console.log(copyFileResponse);
+  })
+  .catch((err) => {
+    console.log(err);
   });
+
+// fs.writeFile("./index.html", pageHTML, (err) => {
+//   if (err) throw new Error(err);
+
+//   console.log(
+//     "Page complete! Check out index.html in this directory to see it!");
